9aaec9a28096c9e00b1234f9499c8e2ce87ee178
sancarbar <sancabar@gmail.com>
2024-02-21 09:47:07 -0500

    Implemented new field for note with created date.

--- app/src/main/java/org/ada/school/notesapp/data/AppDatabase.kt
+++ app/src/main/java/org/ada/school/notesapp/data/AppDatabase.kt
@@ -2,9 +2,11 @@ package org.ada.school.notesapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 
 @Database(entities = [Note::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
--- app/src/main/java/org/ada/school/notesapp/data/Note.kt
+++ app/src/main/java/org/ada/school/notesapp/data/Note.kt
@@ -3,12 +3,16 @@ package org.ada.school.notesapp.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.Date
 
 
 @Entity
 data class Note(
     @PrimaryKey(autoGenerate = true) val uid: Int?,
     @ColumnInfo(name = "text") val text: String,
-    @ColumnInfo(name = "color") val color: String
-)
+    @ColumnInfo(name = "color") val color: String,
+    @ColumnInfo(name = "date") val createdAt: Date
+) {
+    constructor(text: String, color: String) : this(null, text, color, Date())
+}
 
